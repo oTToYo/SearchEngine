28be7782e68246af6c3a098873db42ac2c374dd3
Goshawk <xhome@hotmail.com.tw>
2014-09-30 02:01:49 +0800

    add class

---
+++ src/searchEngine.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class searchEngine {
+	public static void main(String args[])
+	{
+		System.out.println("d");
+		
+	}
+	public searchEngine()
+	{
+		
+	}
+}
