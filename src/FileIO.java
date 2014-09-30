80479f6178c3ba9d642f88e9d3589937cebf26b1
Goshawk <xhome@hotmail.com.tw>
2014-10-01 04:20:08 +0800

    add file2Text, text2File,text2Map function

---
+++ src/FileIO.java
@@ -0,0 +1,79 @@
+import java.util.*;
+import java.io.*;
+
+public class FileIO {
+
+	
+	public static void txt2File(String txt,String path,String encode) throws Exception//output file
+	{
+		FileOutputStream out = new FileOutputStream(path);
+		Writer wr;
+		
+		if(encode==null) // 不使用編碼
+			wr = new OutputStreamWriter(out);
+		else
+			wr = new OutputStreamWriter(out,encode);
+		wr.write(replace(txt, "\n", "\r\n"));
+	    wr.close();
+	}
+	public static String replace(String str,String iPat,String oPat)
+	{
+		if(iPat.length()==0) return str; //沒有要取代的pattern
+		if(str.indexOf(str)<0) return str; //str中找不到要取代pattern
+		
+		StringBuffer outStr = new StringBuffer();
+		int start = 0,next;
+		
+		while((next = str.indexOf(iPat,start))>=0) //尋找文中所有要取代的pattern
+		{
+			outStr.append(str.substring(start, next));
+		    outStr.append(oPat);
+		    start = next + iPat.length();	
+		}
+		
+		outStr.append(str.substring(start));
+		return outStr.toString();
+		
+	}
+	public static String file2Text(String fName,String encode) throws Exception
+	{
+		FileInputStream fin= new FileInputStream(fName);
+	    
+		InputStreamReader isr;
+		if(encode == null)
+			isr = new InputStreamReader(fin);
+		else
+			isr = new InputStreamReader(fin, encode);
+		
+		final int  BUFSIZE = 4096;
+		BufferedReader reader=new BufferedReader(isr, BUFSIZE);
+	    StringBuffer outText = new StringBuffer();
+	    do{
+	    	char[] buf = new char[BUFSIZE];
+	        int len = reader.read(buf, 0, BUFSIZE);
+	        if (len > 0) // 檢查是否讀到檔尾
+	        {
+	          String bufStr = new String(buf, 0, len);
+	          outText.append(bufStr);
+	        }
+	        if (len < BUFSIZE) break;
+	      } while (true);
+	    
+	      reader.close();
+	      return replace(outText.toString(), "\r", "");
+	
+	}
+	public static TreeMap text2Map(String txt)
+	{
+		TreeMap map = new TreeMap();
+	    String[] lines = txt.split("\n");
+	    for (int i=0; i<lines.length; i++)
+	    {
+	      String[] tokens=lines[i].split("=");
+	      if (tokens.length >= 2)
+	        map.put(tokens[0].trim(), tokens[1].trim()); //首尾去除空白並存入map
+	    }
+	    return map;
+		
+	}
+}
--- src/searchEngine.java
+++ src/searchEngine.java
@@ -1,14 +1,41 @@
 import java.util.*;
 import java.io.*;
 
-public class searchEngine {
-	public static void main(String args[])
+public class SearchEngine {
+	static final int MAX_HASH=1023;
+	StringBuffer[] wdHashBuf = new StringBuffer[MAX_HASH];
+	String root,listFName,hashFName;
+	RandomAccessFile listFile;
+	TreeMap lstHashMap;
+	public static void main(String args[]) throws Exception
 	{
+		
 		System.out.println("d");
+		SearchEngine engine = new SearchEngine("data/");
 		
 	}
-	public searchEngine()
+	public SearchEngine(String rootAd) throws Exception
 	{
+		root = rootAd;
+		File idxDir = new File(root+"idx");
+		idxDir.mkdirs();
+		listFName = root+"idx\\files.lst";
+		hashFName = root+"idx\\files.hash";
+		
+		File file = new File(listFName);
+		if(!file.exists())
+			FileIO.txt2File("", listFName, "");
+		listFile = new RandomAccessFile(listFName, "rwd");
+		listFile.seek(listFile.length());//移到listFile的尾巴
+		try 
+		{
+			  String hashFileText = FileIO.file2Text(hashFName,"");
+			  lstHashMap = FileIO.text2Map(hashFileText);
+		} 
+		catch (Exception e) { lstHashMap = new TreeMap(); } //如果找不到檔案
+		
+		for (int i=0; i<MAX_HASH; i++)
+			wdHashBuf[i] = new StringBuffer(" ");
 		
 	}
 }
